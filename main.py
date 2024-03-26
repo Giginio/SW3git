5376ad949ab1ec8fd9e43012621fdc5050da8043
Giginio <gigi.margarito1@gmail.com>
2024-03-26 10:27:20 +0100

    First Commit Pierluigi

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create a DataFrame with random numbers
+data = {'A': np.random.rand(50),
+        'B': np.random.rand(50)}
+
+df = pd.DataFrame(data)
+
+# Plot the DataFrame
+plt.figure(figsize=(10, 6))
+plt.scatter(df['A'], df['B'])
+plt.title('Scatter Plot of Random Numbers')
+plt.xlabel('A')
+plt.ylabel('B')
+plt.grid(True)
+plt.show()
